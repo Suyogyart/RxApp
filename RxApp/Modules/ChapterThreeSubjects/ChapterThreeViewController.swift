31a7e43f269541d4a6c05e6476d6f562731c1556
srt <ebpearls@srt.local>
2018-12-04 17:18:45 +0545

    Publish subject

--- RxApp/Modules/ChapterThreeSubjects/ChapterThreeViewController.swift
+++ RxApp/Modules/ChapterThreeSubjects/ChapterThreeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class ChapterThreeViewController: UIViewController {
 
@@ -16,7 +17,54 @@ class ChapterThreeViewController: UIViewController {
         super.viewDidLoad()
 
         title = "Chapter Three - Subjects"
+        
+        runExamples()
 
+    }
+    
+    private func runExamples() {
+        //publish subject
+        //PublishSubject only emits to current subscribers.
+        //So if you weren’t subscribed when something was added to it previously,
+        //you don’t get it when you do subscribe
+        example(of: "publish subject") {
+            let subject = PublishSubject<String>()
+            
+            subject.onNext("Is anyone listening?")
+            
+            let subscriptionOne = subject.subscribe(onNext: { element in
+                print(element)
+            })
+            
+            subject.on(.next("1"))
+            subject.onNext("2")
+            
+            let subscriptionTwo = subject.subscribe({ event in
+                print("2)", event.element ?? event)
+            })
+            
+            subject.onNext("3")
+            
+            subscriptionOne.dispose()
+            
+            subject.onNext("4")
+            
+            subject.onCompleted()
+            
+            subject.onNext("5")
+            
+            subscriptionTwo.dispose()
+            
+            subject.subscribe({
+                print("3)", $0.element ?? $0)
+            }).disposed(by: viewModel.disposeBag)
+            
+            subject.onNext("?")
+            
+        }
+        
+        
+        //Behavior Subject
     }
 
 }
